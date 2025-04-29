9bfd4f6bc5df62e692b3b14384f8ce9ab06f2398
jungeun <angal2310@tukorea.ac.kr>
2025-04-29 19:37:15 +0900

    set : 로컬 env 세팅

--- techeerzip/src/main/java/backend/techeerzip/TecheerzipApplication.java
+++ techeerzip/src/main/java/backend/techeerzip/TecheerzipApplication.java
@@ -2,8 +2,10 @@ package backend.techeerzip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:env.properties")
 public class TecheerzipApplication {
 
 	public static void main(String[] args) {
