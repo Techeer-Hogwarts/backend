1fa07f3b96f5d21426057b7091e0c7e314b1481e
jungeun <angal2310@tukorea.ac.kr>
2025-04-30 23:36:20 +0900

    fix : Role entity 수정

--- techeerzip/src/main/java/backend/techeerzip/domain/role/entity/Role.java
+++ techeerzip/src/main/java/backend/techeerzip/domain/role/entity/Role.java
@@ -4,40 +4,65 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.*;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        name = "\"Role\"",
+        uniqueConstraints = @UniqueConstraint(
+                name = "\"Role_name_key\"",
+                columnNames = "name"
+        )
+)
 public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @SequenceGenerator(
+            name = "role_id_seq_gen",
+            sequenceName = "\"Role_id_seq\"",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "role_id_seq_gen")
+    private Integer id;
 
+    @Column(
+            name = "name",
+            nullable = false,
+            columnDefinition = "text"
+    )
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_id")
+    @JoinColumn(name = "parentId", foreignKey = @ForeignKey(name = "\"Role_parentId_fkey\""))
     private Role parent;
 
+    @CreationTimestamp
+    @Column(
+            name = "createdAt",
+            nullable = false,
+            updatable = false
+    )
     private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(
+            name = "updatedAt",
+            nullable = false
+    )
     private LocalDateTime updatedAt;
-    private boolean isDeleted;
 
     public Role(String name) {
         this.name = name;
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-        this.isDeleted = false;
     }
 
     public Role(String name, Role parent) {
         this.name = name;
         this.parent = parent;
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-        this.isDeleted = false;
     }
 
     public void setParent(Role parent) {
