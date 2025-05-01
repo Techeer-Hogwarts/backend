9ca4201d10cff84dee1d1996b1802a31bb000403
dongwoo <woomacho@gmail.com>
2025-05-02 03:44:41 +0900

    test: projectTeam entity test

--- techeerzip/src/test/java/backend/techeerzip/domain/projectMember/entity/ProjectMemberTest.java
+++ techeerzip/src/test/java/backend/techeerzip/domain/projectMember/entity/ProjectMemberTest.java
@@ -1,26 +1,26 @@
-/*
 package backend.techeerzip.domain.projectMember.entity;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import backend.techeerzip.domain.projectMember.repository.ProjectMemberRepository;
 import backend.techeerzip.domain.projectTeam.entity.ProjectTeam;
 import backend.techeerzip.domain.projectTeam.repository.ProjectTeamRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProjectMemberTest {
 
-    @Autowired private ProjectMemberRepository projectMemberRepository;
-    @Autowired private ProjectTeamRepository projectTeamRepository;
+    @Autowired
+    private ProjectMemberRepository projectMemberRepository;
+    @Autowired
+    private ProjectTeamRepository projectTeamRepository;
 
     @Test
     void update() {
         ProjectTeam projectTeam =
                 ProjectTeam.builder()
                         .projectExplain("")
-                        .name("1")
+                        .name("11")
                         .recruitExplain("")
                         .notionLink("")
                         .isRecruited(true)
@@ -32,7 +32,6 @@ class ProjectMemberTest {
                         .backendNum(1)
                         .isFinished(false)
                         .build();
-        ProjectTeam created = projectTeamRepository.save(projectTeam);
+        projectTeamRepository.save(projectTeam);
     }
 }
-*/
