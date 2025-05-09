e7a2853078aa6596541d3a04d92ab3bff2343f30
jungeun <angal2310@tukorea.ac.kr>
2025-05-10 02:48:58 +0900

    fix : 코드 커버리지 생성 및 스웨거 설정 수정

--- techeerzip/src/main/java/backend/techeerzip/global/config/WebConfig.java
+++ techeerzip/src/main/java/backend/techeerzip/global/config/WebConfig.java
@@ -10,18 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                // .allowedOrigins(
-                //     "https://www.techeerzip.cloud", 
-                //     "https://api.techeerzip.cloud", 
-                //     "https://*.yje.kr",
-                //     )
                 .allowedOriginPatterns(
                         "http://localhost:*",
                         "http://127.0.0.1:*",
                         "https://*-techeerzip.vercel.app",
                         "https://*.yje.kr",
                         "https://www.techeerzip.cloud",
-                        "https://api.techeerzip.cloud")
+                        "https://api.techeerzip.cloud",
+                        "https://*.techeerzip.cloud")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
