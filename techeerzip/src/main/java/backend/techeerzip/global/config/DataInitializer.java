ae94881b0750e77d1c84d1b0d82ddefafe60b5c0
yoonjungeun1 <angal2310@tukorea.ac.kr>
2025-05-10 00:44:24 +0900

    fix : 테스트 코드 설정 수정

--- techeerzip/src/main/java/backend/techeerzip/global/config/DataInitializer.java
+++ techeerzip/src/main/java/backend/techeerzip/global/config/DataInitializer.java
@@ -16,6 +16,7 @@ import backend.techeerzip.global.logger.CustomLogger;
 import lombok.RequiredArgsConstructor;
 
 @Component
+@Profile("!test")
 @RequiredArgsConstructor
 public class DataInitializer {
 
