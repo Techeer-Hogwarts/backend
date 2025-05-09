1e52665068fda7950324b5c4a5c2884df1dd5e91
yoonjungeun1 <angal2310@tukorea.ac.kr>
2025-05-10 01:37:49 +0900

    fix: 테스트코드 수정

--- techeerzip/src/test/java/backend/techeerzip/TecheerzipApplicationTests.java
+++ techeerzip/src/test/java/backend/techeerzip/TecheerzipApplicationTests.java
@@ -2,10 +2,10 @@ package backend.techeerzip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@TestPropertySource("classpath:application-test.properties")
 class TecheerzipApplicationTests {
 
     @Test
