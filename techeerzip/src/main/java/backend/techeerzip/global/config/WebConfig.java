40368d7d89258a49dd807495ed2abc0417b36aa1
jungeun yoon <angal2310@tukorea.ac.kr>
2025-05-10 18:06:03 +0900

    set: jacoco github actions 세팅 (#3) BACKEND-60
    * set: jacoco github actions 세팅
    
    * fix: 잘못된 경로 수정
    
    * fix: 빌드 오류 해결
    
    * fix: 테스트코드 수정
    
    * fix : gitignore
    
    * fix : 테스트 코드 설정 수정
    
    * fix : ci 수정
    
    * fix: 테스트코드 수정
    
    * fix: 테스트코드 수정
    
    * fix: 테스트코드 수정
    
    * fix: 테스트코드 수정
    
    * fix: 테스트코드 수정
    
    * set : 테스트 코드 수정
    
    * fix : application-test.properties 수정
    
    * fix: ci 수정
    
    * fix : ci 수정
    
    * fix : ci 수정
    
    * fix : ci 수정
    
    * fix : ci 수정
    
    * fix : ci 수정
    
    * set : ci 수정
    
    * fix : CORS 설정 수정
    
    * fix : 코드 커버리지 생성 및 스웨거 설정 수정
    
    * fix : ci 수정
    
    * fix : swagger config 경로 지정
    
    * set : 스웨거 설정 추가
    
    * fix : 불필요한 코드 제거

--- techeerzip/src/main/java/backend/techeerzip/global/config/WebConfig.java
+++ techeerzip/src/main/java/backend/techeerzip/global/config/WebConfig.java
@@ -1,6 +1,8 @@
 package backend.techeerzip.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,14 +12,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://www.techeerzip.cloud", "https://api.techeerzip.cloud")
                 .allowedOriginPatterns(
                         "http://localhost:*",
                         "http://127.0.0.1:*",
                         "https://*-techeerzip.vercel.app",
                         "https://*.yje.kr",
                         "https://www.techeerzip.cloud",
-                        "https://api.techeerzip.cloud")
+                        "https://api.techeerzip.cloud",
+                        "https://*.techeerzip.cloud")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
