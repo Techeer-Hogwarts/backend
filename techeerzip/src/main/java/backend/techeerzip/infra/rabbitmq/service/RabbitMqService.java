f1a4f4d4e09c38440dcbdfddc0f7a1c17a2f04e8
jungeun <angal2310@tukorea.ac.kr>
2025-05-12 02:58:12 +0900

    set : RabbitMq 세팅

--- techeerzip/src/main/java/backend/techeerzip/infra/rabbitmq/service/RabbitMqService.java
+++ techeerzip/src/main/java/backend/techeerzip/infra/rabbitmq/service/RabbitMqService.java
@@ -1,19 +1,42 @@
 package backend.techeerzip.infra.rabbitmq.service;
 
-import org.springframework.stereotype.Service;
-
-import backend.techeerzip.global.config.rabbitmq.RabbitMqSender;
-import backend.techeerzip.infra.rabbitmq.type.MqResource;
+import backend.techeerzip.global.logger.CustomLogger;
+import backend.techeerzip.global.config.RabbitMqConfig;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RabbitMqService {
+    private static final String CONTEXT = "RabbitMqService";
+    private final RabbitTemplate rabbitTemplate;
+    private final CustomLogger logger;
 
-    private final RabbitMqSender sender;
-
-    /** 큐에 메시지 전송 */
-    public void sendCrawlTask(String taskId, String task, String type) {
-        sender.send(MqResource.CRAWL, taskId, task, type);
+    /**
+     * 큐에 메시지 전송
+     */
+    public void sendToQueue(String taskId, String task, String type) {
+        try {
+            rabbitTemplate.convertAndSend(
+                RabbitMqConfig.CRAWL_EXCHANGE,
+                RabbitMqConfig.CRAWL_ROUTING_KEY,
+                task,
+                message -> {
+                    message.getMessageProperties().setMessageId(taskId);
+                    message.getMessageProperties().setType(type);
+                    message.getMessageProperties().setContentType("text/plain");
+                    return message;
+                }
+            );
+            logger.debug(String.format("Sent task: %s - type: %s", task, type), CONTEXT);
+        } catch (Exception e) {
+            logger.error(
+                    "Failed to send task: {}, error: {}",
+                    taskId,
+                    e.getMessage()
+            );
+            throw e;
+        }
     }
-}
+} 
