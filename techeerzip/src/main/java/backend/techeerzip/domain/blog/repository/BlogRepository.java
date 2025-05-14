47c0ec7ba149222c7cb10970cf0041ee6f3f6555
jungeun <angal2310@tukorea.ac.kr>
2025-05-15 03:49:29 +0900

    feat : 블로그 레포지토리 구현

--- techeerzip/src/main/java/backend/techeerzip/domain/blog/repository/BlogRepository.java
+++ techeerzip/src/main/java/backend/techeerzip/domain/blog/repository/BlogRepository.java
@@ -1,11 +1,23 @@
 package backend.techeerzip.domain.blog.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import backend.techeerzip.domain.blog.entity.Blog;
+import backend.techeerzip.domain.blog.entity.BlogCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 
 public interface BlogRepository extends JpaRepository<Blog, Long> {
-    List<Blog> findByUserId(Long userId);
+    Page<Blog> findByIsDeletedFalseAndCategoryOrderByCreatedAtDesc(String category, Pageable pageable);
+
+    Page<Blog> findByIsDeletedFalseAndUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
+    
+    @Query("SELECT b FROM Blog b WHERE b.isDeleted = false AND b.date >= :twoWeeksAgo ORDER BY b.viewCount DESC")
+    Page<Blog> findBestBlogs(@Param("twoWeeksAgo") LocalDateTime twoWeeksAgo, Pageable pageable);
+    
+    @Query("SELECT b FROM Blog b WHERE b.isDeleted = false AND b.id = :blogId")
+    Blog findByIdAndIsDeletedFalse(@Param("blogId") Long blogId);
 }
