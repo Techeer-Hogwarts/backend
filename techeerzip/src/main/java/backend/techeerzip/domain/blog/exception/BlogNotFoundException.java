d694258928c30efa88ff268652da3f20b19527ea
jungeun <angal2310@tukorea.ac.kr>
2025-05-15 03:54:24 +0900

    feat : Exception

---
+++ techeerzip/src/main/java/backend/techeerzip/domain/blog/exception/BlogNotFoundException.java
@@ -0,0 +1,11 @@
+package backend.techeerzip.domain.blog.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class BlogNotFoundException extends RuntimeException {
+    public BlogNotFoundException() {
+        super("블로그를 찾을 수 없습니다.");
+    }
+} 
