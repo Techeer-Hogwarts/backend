f5cff309e62aab2228ebbdc658126b1a7a855f2c
dongwoo <woomacho@gmail.com>
2025-05-17 03:28:00 +0900

    test: ProjectMember repository, entity

--- techeerzip/src/test/java/backend/techeerzip/domain/projectMember/entity/ProjectMemberTest.java
+++ techeerzip/src/test/java/backend/techeerzip/domain/projectMember/entity/ProjectMemberTest.java
@@ -1,36 +1,31 @@
 package backend.techeerzip.domain.projectMember.entity;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import backend.techeerzip.domain.projectMember.repository.ProjectMemberRepository;
-import backend.techeerzip.domain.projectTeam.entity.ProjectTeam;
-import backend.techeerzip.domain.projectTeam.repository.ProjectTeamRepository;
+import backend.techeerzip.global.entity.StatusCategory;
 
 @SpringBootTest
 class ProjectMemberTest {
 
     @Autowired private ProjectMemberRepository projectMemberRepository;
-    @Autowired private ProjectTeamRepository projectTeamRepository;
 
     @Test
-    void update() {
-        ProjectTeam projectTeam =
-                ProjectTeam.builder()
-                        .projectExplain("")
-                        .name("11")
-                        .recruitExplain("")
-                        .notionLink("")
-                        .isRecruited(true)
-                        .githubLink("")
-                        .fullStackNum(1)
-                        .frontendNum(1)
-                        .devopsNum(1)
-                        .dataEngineerNum(1)
-                        .backendNum(1)
-                        .isFinished(false)
-                        .build();
-        projectTeamRepository.save(projectTeam);
+    void jpaIsDeletedTrueThenFalseTest() {
+        final boolean isMember =
+                projectMemberRepository.existsByUserIdAndProjectTeamIdAndIsDeletedFalseAndStatus(
+                        10L, 1L, StatusCategory.APPROVED);
+        Assertions.assertFalse(isMember);
+    }
+
+    @Test
+    void jpaIsDeletedFalseThenTrueTest() {
+        final boolean isMember =
+                projectMemberRepository.existsByUserIdAndProjectTeamIdAndIsDeletedFalseAndStatus(
+                        7L, 1L, StatusCategory.APPROVED);
+        Assertions.assertTrue(isMember);
     }
 }
