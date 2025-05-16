8b5a37064d2df39dcd575858d3404faab4d8d1c1
dongwoo <woomacho@gmail.com>
2025-05-17 04:01:01 +0900

    feat: ProjectTeam FacadeService 구현 (인프라 및 외부 의존성 담당)

--- techeerzip/src/main/java/backend/techeerzip/domain/projectTeam/service/ProjectTeamFacadeService.java
+++ techeerzip/src/main/java/backend/techeerzip/domain/projectTeam/service/ProjectTeamFacadeService.java
@@ -5,11 +5,20 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
+import backend.techeerzip.domain.projectTeam.dto.request.EmptyResponse;
+import backend.techeerzip.domain.projectTeam.dto.request.GetTeamQueryRequest;
 import backend.techeerzip.domain.projectTeam.dto.request.ImageRequest;
+import backend.techeerzip.domain.projectTeam.dto.request.ProjectApplicantRequest;
+import backend.techeerzip.domain.projectTeam.dto.request.ProjectTeamApplyRequest;
 import backend.techeerzip.domain.projectTeam.dto.request.ProjectTeamCreateRequest;
 import backend.techeerzip.domain.projectTeam.dto.request.ProjectTeamUpdateRequest;
+import backend.techeerzip.domain.projectTeam.dto.response.ProjectMemberApplicantResponse;
+import backend.techeerzip.domain.projectTeam.dto.response.ProjectTeamDetailResponse;
+import backend.techeerzip.domain.projectTeam.dto.response.TeamGetAllResponse;
 
 public interface ProjectTeamFacadeService {
+
+    ResponseEntity<ProjectTeamDetailResponse> getDetail(Long projectTeamId);
 
     ResponseEntity<Long> create(ImageRequest imageRequest, ProjectTeamCreateRequest request);
 
@@ -19,4 +28,21 @@ public interface ProjectTeamFacadeService {
             MultipartFile mainImage,
             List<MultipartFile> resultImages,
             ProjectTeamUpdateRequest request);
+
+    ResponseEntity<List<TeamGetAllResponse>> getAllProjectAndStudyTeams(
+            GetTeamQueryRequest request);
+
+    ResponseEntity<EmptyResponse> closeRecruit(Long projectTeamId, Long userId);
+
+    ResponseEntity<EmptyResponse> softDeleteTeam(Long projectTeamId, Long userId);
+
+    ResponseEntity<EmptyResponse> applyToProject(ProjectTeamApplyRequest request, Long userId);
+
+    ResponseEntity<List<ProjectMemberApplicantResponse>> getApplicants(Long teamId, Long userId);
+
+    ResponseEntity<EmptyResponse> cancelApplication(Long teamId, Long applicantId);
+
+    ResponseEntity<EmptyResponse> acceptApplicant(ProjectApplicantRequest request, Long userId);
+
+    ResponseEntity<EmptyResponse> rejectApplicant(ProjectApplicantRequest request, Long userId);
 }
--- techeerzip/src/main/java/backend/techeerzip/domain/projectTeam/service/ProjectTeamFacedServiceImpl.java
+++ techeerzip/src/main/java/backend/techeerzip/domain/projectTeam/service/ProjectTeamFacedServiceImpl.java
@@ -1,5 +1,6 @@
 package backend.techeerzip.domain.projectTeam.service;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
@@ -9,17 +10,28 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import backend.techeerzip.domain.common.dto.SlackRequest;
+import backend.techeerzip.domain.projectTeam.dto.request.EmptyResponse;
 import backend.techeerzip.domain.projectTeam.dto.request.GetTeamQueryRequest;
+import backend.techeerzip.domain.projectTeam.dto.request.GetTeamQueryRequest.GetTeamQuery;
 import backend.techeerzip.domain.projectTeam.dto.request.ImageRequest;
+import backend.techeerzip.domain.projectTeam.dto.request.ProjectApplicantRequest;
+import backend.techeerzip.domain.projectTeam.dto.request.ProjectTeamApplyRequest;
 import backend.techeerzip.domain.projectTeam.dto.request.ProjectTeamCreateRequest;
 import backend.techeerzip.domain.projectTeam.dto.request.ProjectTeamUpdateRequest;
+import backend.techeerzip.domain.projectTeam.dto.response.ProjectMemberApplicantResponse;
 import backend.techeerzip.domain.projectTeam.dto.response.ProjectTeamCreateResponse;
+import backend.techeerzip.domain.projectTeam.dto.response.ProjectTeamDetailResponse;
 import backend.techeerzip.domain.projectTeam.dto.response.ProjectTeamGetAllResponse;
 import backend.techeerzip.domain.projectTeam.dto.response.ProjectTeamUpdateResponse;
 import backend.techeerzip.domain.projectTeam.dto.response.StudyTeamGetAllResponse;
 import backend.techeerzip.domain.projectTeam.dto.response.TeamGetAllResponse;
+import backend.techeerzip.domain.projectTeam.dto.response.TeamUnionSliceYoungInfo;
 import backend.techeerzip.domain.projectTeam.exception.ProjectTeamMainImageException;
 import backend.techeerzip.domain.projectTeam.exception.ProjectTeamResultImageException;
+import backend.techeerzip.domain.projectTeam.repository.querydsl.TeamUnionViewDslRepository;
+import backend.techeerzip.domain.projectTeam.type.PositionNumType;
+import backend.techeerzip.domain.projectTeam.type.TeamType;
 import backend.techeerzip.domain.studyTeam.service.StudyTeamService;
 import backend.techeerzip.infra.s3.S3Service;
 import lombok.RequiredArgsConstructor;
@@ -31,6 +43,7 @@ public class ProjectTeamFacedServiceImpl implements ProjectTeamFacadeService {
 
     private final ProjectTeamService projectTeamService;
     private final StudyTeamService studyTeamService;
+    private final TeamUnionViewDslRepository teamUnionViewDslRepository;
     private final S3Service s3Service;
 
     // slack 은 애바?
@@ -52,6 +65,26 @@ public class ProjectTeamFacedServiceImpl implements ProjectTeamFacadeService {
         if (count > 10) {
             throw new ProjectTeamResultImageException();
         }
+    }
+
+    private static boolean isOnlyProject(List<PositionNumType> numTypes) {
+        return !numTypes.isEmpty();
+    }
+
+    private static boolean isOnlyStudy(List<TeamType> teamTypes) {
+        return !teamTypes.isEmpty() && teamTypes.getFirst().equals(TeamType.STUDY);
+    }
+
+    private static List<TeamGetAllResponse> toTeamGetAllResponse(
+            List<ProjectTeamGetAllResponse> projectResponses,
+            List<StudyTeamGetAllResponse> studyResponses) {
+        return Stream.concat(projectResponses.stream(), studyResponses.stream())
+                .sorted(Comparator.comparing(TeamGetAllResponse::getCreatedAt).reversed())
+                .toList();
+    }
+
+    public ResponseEntity<ProjectTeamDetailResponse> getDetail(Long projectTeamId) {
+        return ResponseEntity.ok(projectTeamService.updateViewCountAndGetDetail(projectTeamId));
     }
 
     public ResponseEntity<Long> create(
@@ -101,14 +134,82 @@ public class ProjectTeamFacedServiceImpl implements ProjectTeamFacadeService {
 
     public ResponseEntity<List<TeamGetAllResponse>> getAllProjectAndStudyTeams(
             GetTeamQueryRequest request) {
-        final List<ProjectTeamGetAllResponse> projectResponses =
-                projectTeamService.getAllTeams(request);
-        final List<StudyTeamGetAllResponse> studyResponses = studyTeamService.getAllTeams(request);
+        final GetTeamQuery query = request.toQuery();
+        final List<PositionNumType> numTypes = query.getPositionNumTypes();
+        final List<TeamType> teamTypes = query.getTeamTypes();
+        final Long limit = query.getLimit();
+        final Boolean isRecruited = query.getIsRecruited();
+        final Boolean isFinished = query.getIsFinished();
+        if (isOnlyProject(numTypes)) {
+            return ResponseEntity.ok(
+                    new ArrayList<>(
+                            projectTeamService.getYoungTeams(
+                                    numTypes, isRecruited, isFinished, limit)));
+        }
+        if (isOnlyStudy(teamTypes)) {
+            return ResponseEntity.ok(
+                    new ArrayList<>(
+                            studyTeamService.getYoungTeams(isRecruited, isFinished, limit)));
+        }
+        final TeamUnionSliceYoungInfo views =
+                teamUnionViewDslRepository.fetchSliceBeforeCreatedAtDescCursor(query);
 
-        final List<TeamGetAllResponse> allTeams =
-                Stream.concat(projectResponses.stream(), studyResponses.stream())
-                        .sorted(Comparator.comparing(TeamGetAllResponse::getCreatedAt).reversed())
-                        .toList();
-        return ResponseEntity.ok(allTeams);
+        final List<ProjectTeamGetAllResponse> projectResponses =
+                projectTeamService.getYoungTeamsById(views.projectsId(), isRecruited, isFinished);
+
+        final List<StudyTeamGetAllResponse> studyResponses =
+                studyTeamService.getYoungTeamsById(views.studiesId(), isRecruited, isFinished);
+
+        return ResponseEntity.ok(toTeamGetAllResponse(projectResponses, studyResponses));
     }
+
+    public ResponseEntity<EmptyResponse> closeRecruit(Long projectTeamId, Long userId) {
+        projectTeamService.close(projectTeamId, userId);
+        return ResponseEntity.ok(new EmptyResponse());
+    }
+
+    public ResponseEntity<EmptyResponse> softDeleteTeam(Long projectTeamId, Long userId) {
+        projectTeamService.softDelete(projectTeamId, userId);
+        return ResponseEntity.ok(new EmptyResponse());
+    }
+
+    public ResponseEntity<EmptyResponse> applyToProject(
+            ProjectTeamApplyRequest request, Long userId) {
+        SlackRequest slackRequest = projectTeamService.apply(request, userId);
+        // slack send
+        return ResponseEntity.ok(new EmptyResponse());
+    }
+
+    public ResponseEntity<List<ProjectMemberApplicantResponse>> getApplicants(
+            Long teamId, Long userId) {
+        final List<ProjectMemberApplicantResponse> applicants =
+                projectTeamService.getApplicants(teamId, userId);
+        return ResponseEntity.ok(applicants);
+    }
+
+    public ResponseEntity<EmptyResponse> cancelApplication(Long teamId, Long applicantId) {
+        projectTeamService.cancelApplication(teamId, applicantId);
+        return ResponseEntity.ok(new EmptyResponse());
+    }
+
+    public ResponseEntity<EmptyResponse> acceptApplicant(
+            ProjectApplicantRequest request, Long userId) {
+        final Long teamId = request.teamId();
+        final Long applicantId = request.applicantId();
+        projectTeamService.acceptApplicant(teamId, userId, applicantId);
+        return ResponseEntity.ok(new EmptyResponse());
+    }
+
+    public ResponseEntity<EmptyResponse> rejectApplicant(
+            ProjectApplicantRequest request, Long userId) {
+        final Long teamId = request.teamId();
+        final Long applicantId = request.applicantId();
+        projectTeamService.rejectApplicant(teamId, userId, applicantId);
+        return ResponseEntity.ok(new EmptyResponse());
+    }
+
+    //    public ResponseEntity<List<ProjectUserTeamsResponse>> getUserTeams(Long userId) {
+    //        return ResponseEntity.ok(projectTeamService.getUserTeams(userId));
+    //    }
+
 }
