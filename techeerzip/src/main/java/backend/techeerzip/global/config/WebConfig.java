fca952d9decab502abe9babbcb1aaafd08e50ce7
dongwoo <woomacho@gmail.com>
2025-05-17 00:26:49 +0900

    fix: spotless 포멧 정리

--- techeerzip/src/main/java/backend/techeerzip/global/config/WebConfig.java
+++ techeerzip/src/main/java/backend/techeerzip/global/config/WebConfig.java
@@ -1,8 +1,6 @@
 package backend.techeerzip.global.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
