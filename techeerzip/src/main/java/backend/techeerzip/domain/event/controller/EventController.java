70dad93fb37cfd0fb18db17d6751b1c6356e3221
yuripbong <kyri200350@gmail.com>
2025-05-17 15:56:33 +0900

    feat: Event Controller

--- techeerzip/src/main/java/backend/techeerzip/domain/event/controller/EventController.java
+++ techeerzip/src/main/java/backend/techeerzip/domain/event/controller/EventController.java
@@ -9,6 +9,8 @@ import backend.techeerzip.domain.event.dto.EventDto;
 import backend.techeerzip.domain.event.service.EventService;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v3/events")
 @RequiredArgsConstructor
@@ -26,8 +28,37 @@ public class EventController {
     }
 
     @GetMapping
-    public ResponseEntity<?> getAllEvents() {
-        // TODO: Implement getting all events
+    public ResponseEntity<List<EventDto.Response>> getAllEvents(
+            @RequestParam(required = false) String keyword,
+            @RequestParam(required = false) List<String> category,
+            @RequestParam(defaultValue = "0") int offset,
+            @RequestParam(defaultValue = "10") int limit) {
+
+        List<EventDto.Response> events = eventService.getEventList(keyword, category, offset, limit);
+
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{eventId}")
+    public ResponseEntity<EventDto.Response> getEvent(@PathVariable Long eventId) {
+        return ResponseEntity.ok(eventService.getEvent(eventId));
+    }
+
+    @PatchMapping("/{eventId}")
+    public ResponseEntity<EventDto.Response> updateEvent(
+            @PathVariable Long eventId,
+            @Valid @RequestBody EventDto.Create request,
+            @AuthenticationPrincipal(expression = "id") Long userId) {
+
+        return ResponseEntity.ok(eventService.updateEvent(userId, eventId, request));
+    }
+
+    @DeleteMapping("/{eventId}")
+    public ResponseEntity<EventDto.Response> deleteEvent(
+            @PathVariable Long eventId,
+            @AuthenticationPrincipal(expression = "id") Long userId) {
+
+        eventService.deleteEvent(userId, eventId);
+        return ResponseEntity.noContent().build();
     }
 }
