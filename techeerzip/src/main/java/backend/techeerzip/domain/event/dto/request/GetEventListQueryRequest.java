9c800c04bbe2cd0eacac30f70fd5a8280a2d522b
yuripbong <kyri200350@gmail.com>
2025-05-18 03:12:59 +0900

    feat: get event dto

--- techeerzip/src/main/java/backend/techeerzip/domain/event/dto/request/GetEventListQueryRequest.java
+++ techeerzip/src/main/java/backend/techeerzip/domain/event/dto/request/GetEventListQueryRequest.java
@@ -2,11 +2,15 @@ package backend.techeerzip.domain.event.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetEventListQueryRequest {
 
     @Schema(description = "검색할 키워드")
@@ -17,9 +21,9 @@ public class GetEventListQueryRequest {
 
     @Schema(description = "오프셋", example = "0")
     @Min(0)
-    private Integer offset = 0;
+    private int offset = 0;
 
     @Schema(description = "가져올 개수", example = "10")
     @Min(1)
-    private Integer limit = 10;
+    private int limit = 10;
 }
