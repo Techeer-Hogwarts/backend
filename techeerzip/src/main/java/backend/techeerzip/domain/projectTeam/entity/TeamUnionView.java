4d2b87884b03cb3e2088a20e541ef7b091c2ddb8
dongwoo <woomacho@gmail.com>
2025-05-19 05:07:10 +0900

    fix: TeamUnion 논리 오류 수정
    booleanBuilder 적용

--- techeerzip/src/main/java/backend/techeerzip/domain/projectTeam/entity/TeamUnionView.java
+++ techeerzip/src/main/java/backend/techeerzip/domain/projectTeam/entity/TeamUnionView.java
@@ -1,7 +1,5 @@
 package backend.techeerzip.domain.projectTeam.entity;
 
-import java.util.UUID;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -9,6 +7,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.util.UUID;
 import org.hibernate.annotations.Immutable;
 
 import backend.techeerzip.domain.projectTeam.type.TeamType;
--- techeerzip/src/main/java/backend/techeerzip/domain/projectTeam/repository/querydsl/TeamUnionViewDslRepositoryImpl.java
+++ techeerzip/src/main/java/backend/techeerzip/domain/projectTeam/repository/querydsl/TeamUnionViewDslRepositoryImpl.java
@@ -1,12 +1,16 @@
 package backend.techeerzip.domain.projectTeam.repository.querydsl;
 
+import backend.techeerzip.domain.common.util.DslBooleanBuilder;
+import backend.techeerzip.domain.projectTeam.dto.request.GetTeamsQuery;
+import com.querydsl.core.types.dsl.Expressions;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import jakarta.persistence.EntityManager;
 
+import java.util.UUID;
+import java.util.function.Function;
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.core.types.Projections;
@@ -14,7 +18,6 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import backend.techeerzip.domain.common.repository.AbstractQuerydslRepository;
-import backend.techeerzip.domain.projectTeam.dto.request.GetTeamQueryRequest.GetTeamQuery;
 import backend.techeerzip.domain.projectTeam.dto.response.SliceNextInfo;
 import backend.techeerzip.domain.projectTeam.dto.response.TeamUnionInfo;
 import backend.techeerzip.domain.projectTeam.dto.response.TeamUnionSliceYoungInfo;
@@ -32,20 +35,10 @@ public class TeamUnionViewDslRepositoryImpl extends AbstractQuerydslRepository
         super(TeamUnionView.class, em, factory);
     }
 
-    private static BooleanExpression getCreateAtCondition(
-            UUID viewId, LocalDateTime createdAtCursor) {
-        return TU.isDeleted
-                .isFalse()
-                .and(
-                        TU.createdAt
-                                .lt(createdAtCursor)
-                                .or(TU.createdAt.eq(createdAtCursor).and(TU.globalId.lt(viewId))));
-    }
-
     private static TeamUnionSliceYoungInfo mapToYoungTeam(
             List<TeamUnionInfo> teams, SliceNextInfo last) {
-        List<Long> projects = new ArrayList<>();
-        List<Long> studies = new ArrayList<>();
+        final List<Long> projects = new ArrayList<>();
+        final List<Long> studies = new ArrayList<>();
 
         for (TeamUnionInfo i : teams) {
             if (i.getTeamType().equals(TeamType.PROJECT)) {
@@ -77,34 +70,49 @@ public class TeamUnionViewDslRepositoryImpl extends AbstractQuerydslRepository
         }
         return unionTeams;
     }
-
-    public TeamUnionSliceYoungInfo fetchSliceBeforeCreatedAtDescCursor(GetTeamQuery request) {
-        final UUID globalId = request.getGlobalId();
-        final LocalDateTime createdAtCursor = request.getCreateAtCursor();
-        final Long limit = request.getLimit();
-        final List<TeamType> teamTypes = request.getTeamTypes();
-        final Boolean isRecruited = request.getIsRecruited();
-        final Boolean isFinished = request.getIsFinished();
-
-        final BooleanExpression condition = getCreateAtCondition(globalId, createdAtCursor);
-        if (isFinished != null) {
-            condition.and(TU.isFinished.eq(isFinished));
-        }
-        if (isRecruited != null) {
-            condition.and(TU.isRecruited.eq(isRecruited));
-        }
-        if (teamTypes.size() == 1) {
-            condition.and(TU.teamType.eq(teamTypes.getFirst()));
-        }
-        final List<TeamUnionInfo> teams =
-                select(Projections.fields(TeamUnionInfo.class, TU.globalId, TU.id, TU.teamType))
-                        .where(condition)
-                        .orderBy(TU.createdAt.desc())
-                        .limit(limit + 1)
-                        .fetch();
-        final SliceNextInfo lastInfo = setNextInfo(teams, limit);
-        final List<TeamUnionInfo> fitTeams = ensureMaxSize(teams, limit);
+    public TeamUnionSliceYoungInfo fetchSliceBeforeCreatedAtDescCursor(GetTeamsQuery request) {
+        final BooleanExpression condition = buildSearchCondition(request);
+        final List<TeamUnionInfo> teams = selectTeamUnionInfos(condition, request.getLimit());
+        final SliceNextInfo lastInfo = setNextInfo(teams, request.getLimit());
+        final List<TeamUnionInfo> fitTeams = ensureMaxSize(teams, request.getLimit());
 
         return mapToYoungTeam(fitTeams, lastInfo);
+    }
+
+    private BooleanExpression buildSearchCondition(GetTeamsQuery request) {
+        final BooleanExpression teamTypeExpr =
+                request.getTeamTypes().size() == 1 ? TU.teamType.eq(request.getTeamTypes().getFirst()) : null;
+        return DslBooleanBuilder.builder()
+                .andIfNotNull(buildCursorCondition(request.getGlobalId(), request.getCreateAtCursor()))
+                .andIfNotNull(request.getIsRecruited(), TU.isRecruited::eq)
+                .andIfNotNull(request.getIsFinished(), TU.isFinished::eq)
+                .andIfNotNull(teamTypeExpr)
+                .andIfNotNull(TU.isDeleted.eq(false))
+                .build();
+    }
+    private BooleanExpression buildCursorCondition(UUID globalId, LocalDateTime createdAt) {
+        if (createdAt == null) return Expressions.TRUE;
+
+        if (globalId != null) {
+            return TU.createdAt.lt(createdAt)
+                    .or(TU.createdAt.eq(createdAt).and(TU.globalId.lt(globalId)));
+        }
+        return TU.createdAt.lt(createdAt);
+    }
+    private List<TeamUnionInfo> selectTeamUnionInfos(BooleanExpression condition, Long limit) {
+        return select(Projections.fields(
+                TeamUnionInfo.class,
+                TU.globalId,
+                TU.id,
+                TU.teamType
+        ))
+                .from(TU)
+                .where(condition)
+                .orderBy(TU.createdAt.desc())
+                .limit(limit + 1)
+                .fetch();
+    }
+    private <T> BooleanExpression equalIfNotNull(T value, Function<T, BooleanExpression> fn) {
+        return value != null ? fn.apply(value) : null;
     }
 }
