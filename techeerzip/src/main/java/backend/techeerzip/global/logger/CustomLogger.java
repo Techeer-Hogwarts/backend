a0a5930124515c3796cb5f876e7897753b57f828
kimzini <kimjinhee.dev@gmail.com>
2025-05-20 00:45:58 +0900

    feat: jwt 예외 클래스 추가

--- techeerzip/src/main/java/backend/techeerzip/global/logger/CustomLogger.java
+++ techeerzip/src/main/java/backend/techeerzip/global/logger/CustomLogger.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomLogger {
     private static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_RED = "\u001B[31m";
     private static final String ANSI_GREEN = "\u001B[32m";
     private static final String ANSI_YELLOW = "\u001B[33m";
     private static final String ANSI_BLUE = "\u001B[34m";
