cb2a97916825e03d45a5c82e1d5a0c81715d4b78
kimzini <kimjinhee.dev@gmail.com>
2025-05-26 01:34:41 +0900

    fix: 스웨거 https 설정

--- techeerzip/src/main/java/backend/techeerzip/global/config/SwaggerConfig.java
+++ techeerzip/src/main/java/backend/techeerzip/global/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package backend.techeerzip.global.config;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,9 +11,16 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${https.server.url}")
+    private String httpsServerUrl;
+
+    @Value("${staging.server.url}")
+    private String stagingServerUrl;
 
     @Bean
     public OpenAPI openAPI() {
@@ -27,9 +37,23 @@ public class SwaggerConfig {
         // Security 요청 설정
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("cookieAuth");
 
+        // 서버 URL 설정
+        Server httpsServer = new Server();
+        httpsServer.setUrl(httpsServerUrl);
+        httpsServer.setDescription("techeerzip https 서버입니다.");
+
+        Server stagingServer = new Server();
+        stagingServer.setUrl(stagingServerUrl);
+        stagingServer.setDescription("techeerzip 스테이징 서버입니다.");
+
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8000");
+        localServer.setDescription("techeerzip 로컬 서버입니다.");
+
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("cookieAuth", securityScheme))
                 .addSecurityItem(securityRequirement)
-                .info(info);
+                .info(info)
+                .servers(List.of(httpsServer, stagingServer, localServer));
     }
 }
