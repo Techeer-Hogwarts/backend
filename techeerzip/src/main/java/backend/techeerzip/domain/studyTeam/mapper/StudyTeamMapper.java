43e267e12a513de8ce2a89667bf5a5cd12044b9e
dongwoo <woomacho@gmail.com>
2025-05-25 19:21:10 +0900

    feat: 전체 조회 무한스크롤 최신, 좋아요 수, 조회 수 적용

--- techeerzip/src/main/java/backend/techeerzip/domain/studyTeam/mapper/StudyTeamMapper.java
+++ techeerzip/src/main/java/backend/techeerzip/domain/studyTeam/mapper/StudyTeamMapper.java
@@ -1,6 +1,6 @@
 package backend.techeerzip.domain.studyTeam.mapper;
 
-import backend.techeerzip.domain.projectTeam.dto.response.StudyTeamGetAllResponse;
+import backend.techeerzip.domain.studyTeam.dto.StudySliceTeamsResponse;
 import backend.techeerzip.domain.studyTeam.entity.StudyTeam;
 
 public class StudyTeamMapper {
@@ -9,8 +9,8 @@ public class StudyTeamMapper {
         // static 유틸 클래스
     }
 
-    public static StudyTeamGetAllResponse toGetAllResponse(StudyTeam studyTeam) {
-        return StudyTeamGetAllResponse.builder()
+    public static StudySliceTeamsResponse toGetAllResponse(StudyTeam studyTeam) {
+        return StudySliceTeamsResponse.builder()
                 .id(studyTeam.getId())
                 .name(studyTeam.getName())
                 .studyExplain(studyTeam.getStudyExplain())
