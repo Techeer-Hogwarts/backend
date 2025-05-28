004c11cad1fa0202ed20210bc48440a01bc89cc0
dlwhsk0 <hana.dev.314@gmail.com>
2025-05-28 13:57:05 +0900

    refactor: Session Controller에서 Swagger 어노테이션 제거 및 순서 수정

--- techeerzip/src/main/java/backend/techeerzip/domain/session/controller/SessionController.java
+++ techeerzip/src/main/java/backend/techeerzip/domain/session/controller/SessionController.java
@@ -8,16 +8,13 @@ import backend.techeerzip.domain.session.dto.request.SessionBestListRequest;
 import backend.techeerzip.domain.session.dto.response.SessionBestListResponse;
 import backend.techeerzip.domain.session.dto.response.SessionListResponse;
 import backend.techeerzip.global.resolver.UserId;
-import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
-@Tag(name = "세션", description = "세션 관련 API")
 @RestController
 @RequestMapping("/api/v3/sessions")
 @RequiredArgsConstructor
@@ -27,7 +24,7 @@ public class SessionController implements SessionSwagger {
     @PostMapping
     public ResponseEntity<Long> createSession(
             @RequestBody @Valid SessionCreateRequest request,
-            @Parameter(hidden = true) @UserId Long userId
+            @UserId Long userId
     ) {
         return ResponseEntity.ok(sessionService.createSession(request, userId));
     }
@@ -36,10 +33,15 @@ public class SessionController implements SessionSwagger {
     public ResponseEntity<Void> updateSession(
             @RequestBody @Valid SessionCreateRequest request,
             @PathVariable Long sessionId,
-            @Parameter(hidden = true) @UserId Long userId
+            @UserId Long userId
     ) {
         sessionService.updateSession(request, sessionId, userId);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{sessionId}")
+    public ResponseEntity<SessionResponse> getSessionBySessionId(@PathVariable Long sessionId) {
+        return ResponseEntity.ok(sessionService.getSessionBySessionId(sessionId));
     }
 
     @GetMapping
@@ -48,24 +50,19 @@ public class SessionController implements SessionSwagger {
         return ResponseEntity.ok(sessionService.getAllSessions(request));
     }
 
-    @GetMapping("/{sessionId}")
-    public ResponseEntity<SessionResponse> getSessionBySessionId(@PathVariable Long sessionId) {
-        return ResponseEntity.ok(sessionService.getSessionBySessionId(sessionId));
-    }
-
-    @DeleteMapping("/{sessionId}")
-    public ResponseEntity<Void> deleteSession(
-            @PathVariable Long sessionId,
-            @Parameter(hidden = true) @UserId Long userId
-    ) {
-        sessionService.deleteSession(sessionId, userId);
-        return ResponseEntity.ok().build();
-    }
-
     @GetMapping("/best")
     public ResponseEntity<SessionBestListResponse<SessionResponse>> getAllBestSessions(
             @ParameterObject @Valid SessionBestListRequest request
     ) {
         return ResponseEntity.ok(sessionService.getAllBestSessions(request));
+    }
+
+    @DeleteMapping("/{sessionId}")
+    public ResponseEntity<Void> deleteSession(
+            @PathVariable Long sessionId,
+            @UserId Long userId
+    ) {
+        sessionService.deleteSession(sessionId, userId);
+        return ResponseEntity.ok().build();
     }
 }
