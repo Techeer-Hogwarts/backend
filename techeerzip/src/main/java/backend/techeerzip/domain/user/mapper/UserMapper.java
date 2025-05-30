030b21248cb49328a60cd65fea3c00b492afe63f
kimzini <kimjinhee.dev@gmail.com>
2025-05-30 15:20:50 +0900

    feat: 유저 mapper 경력 설명란 필드 추가

--- techeerzip/src/main/java/backend/techeerzip/domain/user/mapper/UserMapper.java
+++ techeerzip/src/main/java/backend/techeerzip/domain/user/mapper/UserMapper.java
@@ -100,6 +100,7 @@ public class UserMapper {
                                                                         : null)
                                                         .category(exp.getCategory())
                                                         .isFinished(exp.isFinished())
+                                                        .description(exp.getDescription())
                                                         .build())
                                 .toList())
                 .build();
