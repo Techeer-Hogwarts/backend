13b74286411aeeb0b98004504d21abc6c62131d7
yuripbong <kyri200350@gmail.com>
2025-05-30 15:11:38 +0900

    chore: event 도메인 재설정

--- techeerzip/src/main/java/backend/techeerzip/domain/event/repository/EventRepository.java
+++ techeerzip/src/main/java/backend/techeerzip/domain/event/repository/EventRepository.java
@@ -1,24 +1,11 @@
 package backend.techeerzip.domain.event.repository;
 
 import backend.techeerzip.domain.event.entity.Event;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
-    @Query("SELECT e FROM Event e WHERE e.isDeleted = false " +
-            "AND (:keyword IS NULL OR e.title LIKE %:keyword%) " +
-            "AND (:category IS NULL OR e.category IN :category)")
-    List<Event> findByFilters(@Param("keyword") String keyword,
-                              @Param("category") List<String> category,
-                              Pageable pageable);
-
-    java.util.Optional<Event> findByIdAndIsDeletedFalse(Long id);
 }
 
