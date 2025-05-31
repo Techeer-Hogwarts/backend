847356dd55c5f5e1cb67d8d8a86e6c384269fce5
dongwoo <woomacho@gmail.com>
2025-06-01 01:17:20 +0900

    fix: 안 쓰는 파일들 삭제

--- techeerzip/src/main/java/backend/techeerzip/domain/projectTeam/dto/response/TeamLeaderAlertData.java
+++
@@ -1,5 +0,0 @@
-package backend.techeerzip.domain.projectTeam.dto.response;
-
-import java.util.List;
-
-public record TeamLeaderAlertData(String teamId, String teamName, List<String> leaderEmail) {}
